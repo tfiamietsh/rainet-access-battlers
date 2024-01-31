ae29954ec54514b0ec3cb1b8273a48579015b3e1
tfiamietsh <ya.touka2017@yandex.ru>
2024-02-01 00:39:55 +0300

    Add delayed decorator

--- misc/decorators.py
+++ misc/decorators.py
@@ -1,3 +1,9 @@
+from __future__ import annotations
+from typing import Callable
+
+import pygame.time
+
+
 def singleton(cls: type):
     def wrapper(*args: tuple[any, ...], **kwargs: dict[str, any]):
         if cls not in __instances:
@@ -5,3 +11,17 @@ def singleton(cls: type):
         return __instances[cls]
     __instances = {}
     return wrapper
+
+
+def delayed(delay: int | Callable[[], int], delayed_func: Callable[[], None]):
+    def decorator(func: Callable[[any, ...], any]):
+        def wrapper(*args: tuple[any, ...], **kwargs: dict[str, any]):
+            nonlocal __then
+            now = pygame.time.get_ticks()
+            if now - __then >= (delay if isinstance(delay, int) else delay()):
+                __then = now
+                delayed_func()
+            func(*args, **kwargs)
+        __then = pygame.time.get_ticks()
+        return wrapper
+    return decorator
