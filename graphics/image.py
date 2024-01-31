19597bed822c8ea968cc416cde8f00a6fd6ff6a0
tfiamietsh <ya.touka2017@yandex.ru>
2024-02-01 01:38:13 +0300

    Add animated image class

--- graphics/image.py
+++ graphics/image.py
@@ -2,7 +2,8 @@ from __future__ import annotations
 from pathlib import Path
 import pygame
 import PIL.Image
-from misc.types import Vec2f
+from misc.types import Vec2f, Iterable
+from misc.decorators import delayed
 
 
 class Image:
@@ -28,3 +29,29 @@ class ImageLoader:
     @staticmethod
     def load(path: Path) -> Image:
         return Image(PIL.Image.open(path))
+
+
+class AnimatedImage(Iterable):
+    def __init__(self, frames: list[Image], delay_between_ms: int, delay_after_ms: int):
+        super().__init__(len(frames))
+        self.__frames = frames
+        self.__delay_between_ms, self.__delay_after_ms = delay_between_ms, delay_after_ms
+        AnimatedImage.draw = delayed(self.__delay, self.next)(AnimatedImage.draw)
+
+    def __delay(self) -> int:
+        return self.__delay_after_ms if self._current_idx == self._idx_limit - 1 else self.__delay_between_ms
+
+    @property
+    def size(self) -> Vec2f:
+        return self.__frames[0].size
+
+    @property
+    def current_frame(self) -> Image:
+        return self.__frames[self._current_idx]
+
+    def resized(self, scale_factor: Vec2f) -> AnimatedImage:
+        return AnimatedImage([frame.resized(scale_factor) for frame in self.__frames],
+                             self.__delay_between_ms, self.__delay_after_ms)
+
+    def draw(self, surface: pygame.Surface, pos: Vec2f):
+        self.current_frame.draw(surface, pos)
