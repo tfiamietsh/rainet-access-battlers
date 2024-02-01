16410742d304054d5eee89c265df53e0ab4a3e56
tfiamietsh <ya.touka2017@yandex.ru>
2024-02-01 19:32:54 +0300

    Add image manager

--- misc/managers.py
+++ misc/managers.py
@@ -1,4 +1,6 @@
+from __future__ import annotations
 from misc.types import *
+from graphics.image import Image, AnimatedImage
 
 
 class ResolutionManager(dict, Iterable):
@@ -14,3 +16,27 @@ class ResolutionManager(dict, Iterable):
     @property
     def current_resolution(self) -> Resolution:
         return self[self._current_idx]
+
+
+class ImageManager(dict):
+    def __init__(self, resolution_manager: ResolutionManager):
+        super().__init__(self)
+        default_resolution_scale_factor = Vec2f(resolution_manager.default_resolution.to_tuple())
+        self.__resolution_manager = resolution_manager
+        self.__resolution_scale_factor_map = {
+            resolution: Vec2f(resolution.to_tuple()) / default_resolution_scale_factor
+            for resolution in resolution_manager.values()
+        }
+
+    def __setitem__(self, name: str, image: Image | AnimatedImage):
+        super().__setitem__(name, {
+            resolution: image.resized(scale_factor)
+            for resolution, scale_factor in self.__resolution_scale_factor_map.items()
+        })
+
+    def __getitem__(self, name: str) -> Image | AnimatedImage:
+        return super().__getitem__(name)[self.__resolution_manager.current_resolution]
+
+    @property
+    def resolution_manager(self) -> ResolutionManager:
+        return self.__resolution_manager
